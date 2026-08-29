49a227f215e8189b4e9079bbc6b4bde6cd785f5c
Ayan-sheikh0 <ayansheikhayan211@gmail.com>
2026-08-29 14:52:31 +0530

    Fix single file GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
   base: '/yusra-birthday/',
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
+  build: {
+    cssCodeSplit: false,
+    assetsInlineLimit: 100000000,
+  },
 })
